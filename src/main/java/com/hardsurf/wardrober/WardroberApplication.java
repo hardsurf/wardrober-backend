ca96a02394c38d2bf1975da461947500cc428384
Danny <liquidcore7@gmail.com>
2019-04-28 11:21:12 +0300

    project skeleton

---
+++ src/main/java/com/hardsurf/wardrober/WardroberApplication.java
@@ -0,0 +1,13 @@
+package com.hardsurf.wardrober;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardroberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardroberApplication.class, args);
+	}
+
+}
